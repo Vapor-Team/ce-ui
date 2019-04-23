8b91f211244a3bf8d8d33c663b37bf6b14af1cce
mark <sund@haidilao.com>
2019-04-23 11:21:50 +0800

    test cz-contventional-changelog

--- packages/index.js
+++ packages/index.js
@@ -1,37 +1,37 @@
 /* eslint-disable */
 // This file is auto gererated by build/build-entry.js
-import BaseCard from './components/base-card'
-import Button from './components/button'
-import Icon from './components/icon'
-import Loading from './components/loading'
-import TextCard from './components/text-card'
+import BaseCard from './base-card'
+import Button from './button'
+import Icon from './icon'
+import Loading from './loading'
+import TextCard from './text-card'
 const version = '0.0.1'
 const components = [
-	BaseCard,
-	Button,
-	Icon,
-	Loading,
-	TextCard
+  BaseCard,
+  Button,
+  Icon,
+  Loading,
+  TextCard
 ]
 const install = Vue => {
-	components.forEach(Component => {
-		Vue.use(Component)
-	})
+  components.forEach(Component => {
+    Vue.use(Component)
+  })
 };
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
-	install(window.Vue)
+  install(window.Vue)
 }
 export {
-	install,
-	version,
-	BaseCard,
-	Button,
-	Icon,
-	Loading,
-	TextCard
+  install,
+  version,
+  BaseCard,
+  Button,
+  Icon,
+  Loading,
+  TextCard
 }
 export default {
-	install,
-	version
+  install,
+  version
 }
