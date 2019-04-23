8c1b194021218cc7e34f80dce292a57883316f88
mark <sund@haidilao.com>
2019-04-23 14:12:42 +0800

    1.fix duplicate style references in document code

--- examples/demos/icon.vue
+++ examples/demos/icon.vue
@@ -1,13 +1,14 @@
 <template>
 	<div class="demo-block">
 		<ul class="icon-list">
-			<li v-for="(item,key) in icons"
-			    :key="key"
-			    @mouseenter="onMouseenter(key)"
-			    @mouseleave="onMouseleave(key)">
+			<li
+				v-for="(item,key) in icons"
+				:key="key"
+				@mouseenter="onMouseenter(key)"
+				@mouseleave="onMouseleave(key)"
+			>
 				<div class="demo-icon-wrap">
-					<ce-icon :name="item.name.substring(8)"
-					         :size="item.size"></ce-icon>
+					<ce-icon :name="item.name.substring(8)" :size="item.size"></ce-icon>
 					<span class="name">{{item.name.substring(8)}}</span>
 				</div>
 			</li>
@@ -45,16 +46,15 @@ export default {
 .icon-list
 	overflow hidden
 	list-style none !important
-	padding 0
+	padding 0 !important
 	border solid 1px #eaeefb
 	border-radius 4px
-
 .icon-list li
 	float left
 	width 16.66%
 	text-align center
 	height 120px
-	line-height 120px
+	line-height 120px !important
 	color #666
 	font-size 13px
 	transition color 0.15s linear
@@ -62,21 +62,17 @@ export default {
 	border-bottom 1px solid #eee
 	margin-right -1px
 	margin-bottom -1px
-
 	.demo-icon-wrap
 		display inline-block
 		line-height normal
 		vertical-align middle
 		font-family 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif
 		color #626673
-
 		.big
 			font-size 64px
-
 	.ce-icon
 		display block
 		margin-bottom 15px
-
 	&:hover
 		color rgb(92, 182, 255)
 </style>
