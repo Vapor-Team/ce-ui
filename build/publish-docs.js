33509103b7bdfe8eb27e546aa6f0d96b53735199
mark <sunduo3195@qq.com>
2019-05-06 19:48:11 +0800

    fix push bug

--- build/publish-docs.js
+++ build/publish-docs.js
@@ -5,22 +5,25 @@ const VERSION = require("../package.json").version
 const GIT_COMMIT = execSync("git rev-parse --short HEAD")
 	.toString()
 	.replace(/\n/, "")
-const CURRENT_BRANCH = execSync("git symbolic-ref --short -q HEAD").toString().replace(/\n/, "")
-const PUB_BRANCH = "publish-docs"
-execSync(`git checkout ${PUB_BRANCH}`)
-execSync("git merge develop --allow-unrelated-histories ")
-execSync("npm run build:docs")
-execSync(`git add . && git commit -m '[deploy] ${GIT_COMMIT} - [release] ${VERSION}'`)
-execSync("git subtree push --prefix dist github gh-pages")
-execSync(`git checkout ${CURRENT_BRANCH}`)
+// const CURRENT_BRANCH = execSync('git symbolic-ref --short -q HEAD').toString().replace(/\n/, '')
+// const PUB_BRANCH = 'publish-docs'
+// execSync(`git checkout ${PUB_BRANCH}`)
+// execSync(`git merge develop --allow-unrelated-histories `)
+// execSync('npm run build:docs')
+// execSync(`git add . && git commit -m \"[deploy] ${GIT_COMMIT} - [release] ${VERSION}\"`)
+// execSync('git subtree push --prefix dist github gh-pages')
+// execSync(`git checkout ${CURRENT_BRANCH}`)
 
 const ghpages = require("gh-pages")
 execSync("npm run build:docs")
-ghpages.publish("dist", {
+ghpages.publish("./dist", {
 	user: {
-		name: "js-mark",
+		name: "JS-mark",
 		email: "sunduo3195@qq.com"
 	},
+	branch: "gh-page",
 	repo: "https://github.com/Vapor-Team/ce-ui.git",
 	message: `[deploy] ${GIT_COMMIT} - [release] ${VERSION}`
+}, (e) => {
+	console.log(e)
 })
