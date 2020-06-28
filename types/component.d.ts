f1f570febd52f28c74d812f4ea9f14e48c030430
mark <sunduo3195@qq.com>
2020-06-28 22:16:20 +0800

    🐛fix(project): fix project ts support bug

--- types/component.d.ts
+++ types/component.d.ts
@@ -1,13 +1,4 @@
-import Vue from 'vue'
-
-/** CeUI component common definition */
-export declare class CeUIComponent extends Vue {
-  /** Install component into Vue */
-  static install(vue: typeof Vue): void
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
 }
-
-/** Component size definition for button, input, etc */
-export type CeUIComponentSize = 'large' | 'medium' | 'small' | 'mini'
-
-/** Horizontal alignment */
-export type CeUIHorizontalAlignment = 'left' | 'center' | 'right'
