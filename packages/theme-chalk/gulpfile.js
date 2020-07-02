7943d191ff9f8be5fa3300f819b7bda3b0f60ec6
mark <sunduo3195@qq.com>
2020-07-02 18:01:19 +0800

    🐛fix(theme-style): fix postcss-bem config

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -10,6 +10,8 @@ const presetenv = require('postcss-preset-env')
 const rename = require('gulp-rename')
 const toBem = require('postcss-bem-fix')
 let bemConfig = {
+  defaultNamespace: 'ce', // default namespace to use, none by default
+  style: 'bem', // suit or bem, suit by default,
   shortcuts: {
     component: 'b',
     modifier: 'm',
