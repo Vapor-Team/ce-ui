3a6be82dfc7222578aa0bed08d4d7c363c8581df
mark <sunduo3195@qq.com>
2020-07-13 02:00:22 +0800

    ✨feat(components): 1.Modify the icon resource file as an open source project
    Modify the language switching function of the project in conjunction with routing optimization

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -22,133 +22,176 @@ const bemConfig = {
     modifier: '--'
   }
 }
-
-function compileCssToVw(done) {
-  return gulp
-    .src('./src/*.styl')
-    .pipe(stylus())
-    .pipe(
-      postcss([
-        toBem(bemConfig),
-        pxToViewPort({
-          viewportWidth: 750, // (Number) The width of the viewport.
-          viewportHeight: 1334, // (Number) The height of the viewport.
-          unitPrecision: 3, // (Number) 转换的时候除不尽保留3位小数.
-          viewportUnit: 'vw', // (String) 转换为vw单位.
-          selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
-          minPixelValue: 1, // (Number) 小于或等于'1px'不转换为视窗单位.
-          mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
-          unitToConvert: 'px'
-        }),
-        presetenv(),
-        cssnano({
-          'cssnano-preset-advanced': {
-            zIndex: false,
-            autoprefixer: true
-          }
+const compile = {
+  /**
+   * 编译vw单位文件
+   */
+  cssToVw() {
+    return gulp
+      .src('./src/*.styl')
+      .pipe(stylus())
+      .pipe(
+        postcss([
+          toBem(bemConfig),
+          pxToViewPort({
+            viewportWidth: 750, // (Number) The width of the viewport.
+            viewportHeight: 1334, // (Number) The height of the viewport.
+            unitPrecision: 3, // (Number) 转换的时候除不尽保留3位小数.
+            viewportUnit: 'vw', // (String) 转换为vw单位.
+            selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+            minPixelValue: 1, // (Number) 小于或等于'1px'不转换为视窗单位.
+            mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
+            unitToConvert: 'px'
+          }),
+          presetenv(),
+          cssnano({
+            'cssnano-preset-advanced': {
+              zIndex: false,
+              autoprefixer: true
+            }
+          })
+        ])
+      )
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          suffix: '.vw'
         })
-      ])
-    )
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        suffix: '.vw'
-      })
-    )
-    .pipe(gulp.dest('./lib'))
-}
-
-function compileCssToPx(done) {
-  return gulp
-    .src('./src/*.styl')
-    .pipe(stylus())
-    .pipe(
-      postcss([
-        toBem(bemConfig),
-        presetenv(),
-        pxToUnits({
-          divisor: 1,
-          targetUnits: 'px'
+      )
+      .pipe(gulp.dest('./lib'))
+  },
+  /**
+   * 编译px单位文件
+   */
+  cssToPx() {
+    return gulp
+      .src('./src/*.styl')
+      .pipe(stylus())
+      .pipe(
+        postcss([
+          toBem(bemConfig),
+          presetenv(),
+          pxToUnits({
+            divisor: 1,
+            targetUnits: 'px'
+          })
+        ])
+      )
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          suffix: '.px'
         })
-      ])
-    )
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        suffix: '.px'
-      })
-    )
-    .pipe(gulp.dest('./lib'))
-}
-
-function compileCssToRelease(done) {
-  return gulp
-    .src('./src/*.styl')
-    .pipe(stylus())
-    .pipe(
-      postcss([
-        toBem(bemConfig),
-        presetenv(),
-        pxToUnits({
-          divisor: 1,
-          targetUnits: 'px'
+      )
+      .pipe(gulp.dest('./lib'))
+  },
+  /**
+   * build css
+   */
+  cssToRelease() {
+    return gulp
+      .src('./src/*.styl')
+      .pipe(stylus())
+      .pipe(
+        postcss([
+          toBem(bemConfig),
+          presetenv(),
+          pxToUnits({
+            divisor: 1,
+            targetUnits: 'px'
+          })
+        ])
+      )
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          extname: '.css'
         })
+      )
+      .pipe(gulp.dest('./lib'))
+  },
+  /**
+   * dev css
+   */
+  cssToDev() {
+    return gulp
+      .src([
+        './src/common/**/*.styl',
+        './src/components/**/*.styl',
+        './src/*.styl'
       ])
-    )
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        extname: '.css'
-      })
-    )
-    .pipe(gulp.dest('./lib'))
-}
-
-function compileCssToDev(done) {
-  return gulp
-    .src('./src/**/*.styl')
-    .pipe(stylus())
-    .pipe(
-      postcss([
-        toBem(bemConfig),
-        presetenv(),
-        pxToUnits({
-          divisor: 1,
-          targetUnits: 'px'
+      .pipe(stylus())
+      .pipe(
+        postcss([
+          toBem(bemConfig),
+          presetenv(),
+          pxToUnits({
+            divisor: 1,
+            targetUnits: 'px'
+          })
+        ])
+      )
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          extname: '.css'
         })
-      ])
-    )
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        extname: '.css'
-      })
-    )
-    .pipe(gulp.dest('./lib'))
-}
-function copyFont(done) {
-  return gulp.src('./src/fonts/**').pipe(gulp.dest('./lib/fonts'))
-}
-// 监视构建
-function watchCss(done) {
-  return gulp.watch('./src/**', gulp.parallel(compileCssToDev))
+      )
+      .pipe(gulp.dest('./lib'))
+  },
+  /**
+   * 处理font
+   */
+  copyFont() {
+    return gulp.src('./src/fonts/**').pipe(gulp.dest('./lib/fonts'))
+  },
+  /**
+   * 处理fontJS
+   */
+  copyFontJS() {
+    return gulp.src('./src/components/icon/*.js').pipe(gulp.dest('./lib/icon'))
+  }
 }
 
-function watchFonts(done) {
-  return gulp.watch('./src/fonts/**', copyFont)
+const watch = {
+  /**
+   * 监视构建css
+   */
+  css() {
+    return gulp.watch(
+      [
+        './src/common/**',
+        './src/common/**',
+        './src/components/**',
+        './src/mixins/**',
+        './src/*.styl'
+      ],
+      gulp.parallel(compile.cssToDev)
+    )
+  },
+  /**
+   * 监听字体库
+   */
+  fonts() {
+    return gulp.watch('./src/fonts/**', compile.copyFont)
+  },
+  fontJS() {
+    return gulp.watch('./src/fonts/**', compile.copyFontJS)
+  }
 }
 
 exports.build = gulp.parallel(
   // compileCssToVw, // 构建 vw_css
   // compileCssToPx, // 构建 px_css
-  compileCssToRelease,
-  copyFont
+  compile.cssToRelease,
+  compile.copyFont,
+  compile.copyFontJS
 )
 
 exports.default = gulp.series(
-  // compileCssToVw, // 构建 vw_css
-  // compileCssToPx, // 构建 px_css
-  compileCssToDev,
-  copyFont,
-  gulp.parallel(watchCss, watchFonts)
+  // compile.cssToVw, // 构建 vw_css
+  // compile.cssToPx, // 构建 px_css
+  compile.cssToDev,
+  compile.copyFont,
+  gulp.parallel(watch.css, watch.fonts, watch.fontJS)
 )
